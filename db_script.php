04f9f9df143f547d9b388466fee6b356e4679c88
Zachary Mays <zackmays@gmail.com>
2016-05-29 13:58:58 -0400

    Adds readme and database script

---
+++ db_script.php
@@ -0,0 +1,62 @@
+ <?php
+$servername = "localhost:8889";
+$username = "root";
+$password = "root";
+$dbname = "yardsley_test";
+
+try
+{
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+    $create_db = "CREATE DATABASE IF NOT EXISTS `yardsley_test` DEFAULT CHARACTER SET latin1 					COLLATE latin1_swedish_ci;
+					USE `yardsley`";
+					
+    $conn->exec($create_db);
+	
+    $conn2 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);	
+
+					
+	$create_poducts_table = "CREATE TABLE `products` (
+							  `id` int(11) NOT NULL,
+							  `user_id` int(11) unsigned NOT NULL,
+							  `name` varchar(255) NOT NULL,
+							  `description` text,
+							  `photo` varchar(255) NOT NULL DEFAULT 'generic',
+							  `category` varchar(255) NOT NULL,
+							  `brand` varchar(255) NOT NULL,
+							  `original_price` int(11) NOT NULL,
+							  `asking_price` int(11) NOT NULL,
+							  `created_at` datetime NOT NULL,
+							  `updated_at` datetime NOT NULL
+							) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+	$create_users_table = "CREATE TABLE `users` (
+							  `id` int(11) NOT NULL,
+							  `name` varchar(255) NOT NULL,
+							  `email` varchar(255) NOT NULL,
+							  `created_at` datetime NOT NULL,
+							  `updated_at` datetime NOT NULL
+							) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+	
+	$add_products_key = "ALTER TABLE `products` ADD PRIMARY KEY (`id`), ADD KEY `products_user_id_foreign` (`user_id`) USING BTREE";
+	$add_users_key = "ALTER TABLE `users` ADD PRIMARY KEY (`id`)";
+	$auto_increment_product_id = "ALTER TABLE `products` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT";
+	$auto_increment_user_id = "ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT";
+	
+    $conn2->exec($create_poducts_table);
+    $conn2->exec($create_users_table);
+    $conn2->exec($add_products_key);
+    $conn2->exec($add_users_key);
+    $conn2->exec($auto_increment_product_id);
+    $conn2->exec($auto_increment_user_id);
+    echo "Database set up successfully";
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
+
+$conn = null;
+?> 
---
+++ readme.txt
@@ -0,0 +1,15 @@
+# Welcome to Yardsley
+
+Yardsley is a web app that allows users to upload products for sale and browse products uploaded by other users.
+
+It is built using [Ember.js](http://emberjs.com/) for the front-end and the [Slim Framework](http://www.slimframework.com/) on the backend. To run this app locally, follow these steps:
+
+* Download the repository into a new directory.
+* Update `db_script.php` with your local MySQL settings (you do not need to create the actual database ahead of time).
+* On the command line, navigate to the directory containing your project and run `php db_script.php`.
+* On the command line, run `php -S localhost:8000` to start a PHP server.
+* Open a web browser and navigate to `http://localhost:8000`.
+
+You should now see the Yardsley homepage - you're ready to get started!
+
+View this repository online at https://github.com/jackel414/yardsley
